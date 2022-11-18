ca839e1f3d938309a8a647f4ef8927e17789e0c5
parralol <santinico36@hotmail.com>
2022-11-18 12:05:57 -0500

    "paquetes"

---
+++ src/persistence/ReplicateIO.java
@@ -0,0 +1,45 @@
+package persistence;
+import java.io.*;
+import domain.*;
+
+import domain.ReplicateException;
+public class ReplicateIO {
+    
+    public void abrir00(File file) throws ReplicateException{
+        try{
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }catch(ReplicateException e){
+
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }
+
+    }
+    
+    public void guardar00(File file) throws ReplicateException{
+        try{
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }catch(ReplicateException e){
+
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }
+
+    }
+    public void importar(File file) throws ReplicateException{
+        try{
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }catch(ReplicateException e){
+
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }
+
+    }
+    public void exportar(File file) throws ReplicateException{
+        try{
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }catch(ReplicateException e){
+
+            throw new ReplicateException(ReplicateException.OPCION_EN_CONSTRUCCION);
+        }
+
+    }
+}
