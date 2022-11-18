efeb4cce9ea09e52b0ec828bf8fda6ee8d0334a4
parralol <santinico36@hotmail.com>
2022-11-18 12:21:44 -0500

    "sy"

--- src/domain/replicateException.java
+++ src/domain/replicateException.java
@@ -1,7 +1,7 @@
 package domain;
 
 public class ReplicateException extends Exception{
-    public static  String OPCION_EN_CONSTRUCCION = "Opcion en construccion";
+    public  static final String OPCION_EN_CONSTRUCCION = "Opcion en construccion";
 
 
     public ReplicateException(String message){
